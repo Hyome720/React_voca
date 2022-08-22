ae796502378c8196d64b84f4073be052b9423a5b
Hyormone <ddf998@gmail.com>
2022-08-22 22:55:44 +0900

    version check

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Day from "./component/Day";
 import DayList from "./component/DayList";
 import Header from "./component/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <DayList />
-      <Day />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<DayList />} />
+          <Route path="/day/:day" element={<Day />} />
+          <Route path="*" element={<EmptyPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
